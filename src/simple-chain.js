329eb647cc62a2e702713d6e173491f9ccdc49ae
VulpisxD <vlad30303026@gmail.com>
2021-03-14 17:13:39 +0300

    feat: update

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,21 +1,29 @@
 const CustomError = require("../extensions/custom-error");
-
+let arr = [];
 const chainMaker = {
   getLength() {
-  
+    return arr.length;
   },
   addLink(value) {
-   
+    if (value === undefined) value = "( )";
+    arr.push(`( ${value} )`);
+    return this;
   },
   removeLink(position) {
-   
+    if (typeof(position) !== "number") {arr.length = 0; throw new Error()}
+    if (Number.isInteger(position) === false) {arr.length = 0; throw new Error()}
+    if (position < 0 || position >= arr.length) {arr.length = 0; throw new Error()}
+    arr.splice(position-1, 1);
+    return this;
   },
   reverseChain() {
-   
+    arr.reverse();
+    return this;
   },
   finishChain() {
-    
+    let str = arr.join("~~");
+    arr.length = 0;
+    return str;
   }
 };
-
 module.exports = chainMaker;
